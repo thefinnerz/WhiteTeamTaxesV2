c21632564116c5516808d4fc5c3e750c8417ed17
finnerz <fin.roy1@gmail.com>
2023-10-27 15:00:57 +0100

    checking jenkins webhook

--- src/test/java/org/example/TaxesTest.java
+++ src/test/java/org/example/TaxesTest.java
@@ -26,6 +26,16 @@ class TaxesTest {
     }
 
     @Test
+    void TestCheckBracketUpper() {
+        int[] expectedBracket = {8000,0,0,8000};
+        int[] actualBracket = Taxes.CheckBracket(8000);
+        Assertions.assertEquals(expectedBracket[0], actualBracket[0]);
+        Assertions.assertEquals(expectedBracket[1], actualBracket[1]);
+        Assertions.assertEquals(expectedBracket[2], actualBracket[2]);
+        Assertions.assertEquals(expectedBracket[3], actualBracket[3]);
+    }
+
+    @Test
     void TestCalcTakeHome() {
         int expectedTakeHome = 7000;
         int[] diffAndBracket = {7000,0,0,7000};
